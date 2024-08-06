9c8f101afdfc336c37bb51d8a09e2b79235d32b3
TheCodingPantsula <asmdigital14@gmail.com>
2024-08-06 12:10:33 +0200

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask app for the AirBnB clone API."""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """Close the database at the end of the request."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
